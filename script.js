37a78dae0e9f00f195cd9a757a9f52c38b95bcf5
Eric Camellini <eric.camellini@gmail.com>
2022-12-07 12:36:43 +0100

    Implementing operations

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+// We don't write any code outside functions.
+// BUT -- for storage of data, that needs to be shared
+// between functions...this is ok
+
+// DATA STORAGE
+// (not persistent - if reload the browser, I start from scratch)
+let previousValue;
+let previousOperation;
+// These are called GLOBAL variables, because they are available to all our JS code
+
+// AN ALTERNATIVE...
+// let calculatorStorage = {
+//   previousValue: null,
+//   previousOperation: null
+// }
+
+// _________________________________________
+
 function onNumericButtonClick(eventData) {
   // click listener for numeric buttons
 
@@ -21,6 +39,59 @@ function clearDisplay() {
   document.getElementById("display").innerText = "0";
 }
 
+function onOperationButtonClick(eventData) {
+  let operation = eventData.target.innerText;
+
+  if (operation !== "=") {
+    // We want to
+    // 1) Save the number on the display, and clear it
+    // 2) Save the operation
+    previousValue = document.getElementById("display").innerText;
+    previousOperation = operation;
+    clearDisplay();
+
+    console.log("Memory: ", previousValue, previousOperation);
+  } else {
+    // When we click on equals, we want to
+    // 1) Get the previous number that we saved
+    // 2) Get the number currently on the diplay
+    // 3) Get the operation saved before
+    // 4) Execute the operation --> print the result on display
+
+    // NOTE THAT WE HAVE PREVIOUS VALUE AND PREVIOUS OPERATION AVAIABLE, STORED IN
+    // THE GLOBAL VARS:
+    // previousValue
+    // previousOperation
+    let currentValue = document.getElementById("display").innerText;
+    console.log("Executing: ", previousValue, previousOperation, currentValue);
+
+    // To evaluate the result of a JS operation written as a string
+    // we could use eval()
+    // eval("12 + 14")  ---> 26
+    // BUT -- EVAL SHOULD NEVER BE USED: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/eval
+
+    let result;
+
+    // We need to convert the two things we saved to number.
+    // Why? Because they were both .innerText --> strings.
+    let operand1 = parseFloat(previousValue);
+    let operand2 = parseFloat(currentValue);
+
+    // Let's try with a serie of if
+    if (previousOperation === "+") {
+      result = operand1 + operand2;
+    } else if (previousOperation === "-") {
+      result = operand1 - operand2;
+    } else if (previousOperation === "/") {
+      result = operand1 / operand2;
+    } else if (previousOperation === "*") {
+      result = operand1 * operand2;
+    }
+
+    document.getElementById("display").innerText = result;
+  }
+}
+
 function onLoadOperations() {
   // We want to programmatically add "click" event
   // listeners to all the buttons of our calculator.
@@ -38,6 +109,11 @@ function onLoadOperations() {
     button.addEventListener("click", onNumericButtonClick); // NO (), it will be called later
   } // For OF, not FOR IN. FOR IN is something different
   // it is deprecated, it's buggy feature of JS that it's still there.
+
+  let operationButtons = document.querySelectorAll(".operation-button");
+  for (let button of operationButtons) {
+    button.addEventListener("click", onOperationButtonClick);
+  }
 
   document
     .getElementById("clear-button")
